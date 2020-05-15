f1a2859e8c744a59c73a4f713020df44631b0dcb
Alina Omelchenko <62790604+AlinA414@users.noreply.github.com>
2020-05-15 10:57:27 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,4 +3,4 @@ import styles from './Footer.module.css';
 
 const Footer = ({ count }) => (<p className={styles.text}>Вот сколько дел еще осталось сделать: { count }</p>);
  
- export default Footer;
+export default Footer;
